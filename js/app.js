d79605ce31a9cf2a8fc34933dc4d3d7bda96bfbd
Darius Lee Pasilaban <66649221+pdariuslee@users.noreply.github.com>
2020-07-15 11:07:47 -0700

    Added reference

--- js/app.js
+++ js/app.js
@@ -41,6 +41,7 @@ function CookieStore(location, min, max, averageCookiesPerCustomer) {
     table.appendChild(row);   
     
   };
+  //Code from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   this.getRandomNumberOfCustomerPerHour = function(min, max) {
     
     min = Math.ceil(min);
